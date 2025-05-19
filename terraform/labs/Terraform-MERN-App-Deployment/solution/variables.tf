8d0df679799743d12131ec4d962771a08a3f3d63
Khalid-ALHarthi <159263883+Khalid-ALHarthi@users.noreply.github.com>
2025-05-19 14:00:45 +0000

    new commit

--- terraform/labs/Terraform-MERN-App-Deployment/solution/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  default = "eu-north-1"
-}
-
-variable "key_name" {
-  description = "Name of the SSH key pair to use for instances"
-  type        = string
-  default     = "sda" # please change to your key name
-}
